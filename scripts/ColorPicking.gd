0b38c1258d38e0e11455c01d85d2ebf139ec8f1c
ChristopheClaustre <christophe.claustre.31@gmail.com>
2023-10-01 00:44:39 +0200

    show color in inspector instead of logging each component + ColorPicking doesn't log errors when cursor outside viewport anymore

--- scripts/ColorPicking.gd
+++ scripts/ColorPicking.gd
@@ -1,5 +1,9 @@
 extends Node
-var mainCamera
+var mainCamera : Camera3D
+
+
+@export var color_under_mouse : Color
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,8 +13,9 @@ func _ready() -> void:
 
 func _physics_process(_delta):
 	var cursor = mainCamera.get_viewport().get_mouse_position()
-	var viewportSize = mainCamera.get_viewport().get_visible_rect().size
-	if cursor.x >= 0 and cursor.x < viewportSize.x and cursor.y >= 0 and cursor.y < viewportSize.y:
+	var rect := mainCamera.get_viewport().get_visible_rect()
+	if rect.has_point(cursor):
 		var image = get_viewport().get_texture().get_image()
-		var _color = image.get_pixelv(cursor)
-		#print(color) # Print for debug
+		color_under_mouse = image.get_pixelv(cursor)
+	else:
+		color_under_mouse = Color.BLACK
