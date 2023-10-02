7769d786f23584addbb8292ccd49a73dac664b81
AngryCyboot <cyboot25@gmail.com>
2023-10-02 23:05:32 +0200

    lighting setup

--- scripts/WorldEnvironment.gd
+++ scripts/WorldEnvironment.gd
@@ -14,12 +14,12 @@ func day_night ():
 	if mode:
 		RenderingServer.global_shader_parameter_set("day_night",false)
 		mode = false
-		looks.background_color = Color.BLACK
+		looks.background_color = Color(0.07,0.07,0.07)
 		looks.ambient_light_energy = 0.2
 		looks.ambient_light_color = Color(0.325, 0.494, 1)
 	else:
 		RenderingServer.global_shader_parameter_set("day_night",true)
 		mode = true
-		looks.background_color = Color.WHITE
+		looks.background_color = Color.WHITE_SMOKE
 		looks.ambient_light_energy = 0.5
 		looks.ambient_light_color = Color.WHITE
