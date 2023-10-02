112e04b85e46d81b5e3402a9338c0195aa3f0f8d
AngryCyboot <cyboot25@gmail.com>
2023-10-02 20:01:46 +0200

    wip UI

---
+++ scripts/general_controls.gd
@@ -0,0 +1,21 @@
+extends MarginContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+
+
+func _on_night_day_pressed():
+	print("plop")
+	get_tree().call_group("environement","day_night")
+
+
+func _on_texture_button_toggled(button_pressed):
+	pass
